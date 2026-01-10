0feeefa8896e9da7a96d456e2771ea9a7f2a6c79
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2026-01-10 22:55:27 +0530

    update cors origin to match frontend URL and bump mongoose version and dependencies

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:3000", // Frontend URL
+    origin: "http://localhost:4000", // Frontend URL
     credentials: true
 }));
 app.use(express.json());
